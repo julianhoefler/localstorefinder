210522759c6c7d59a427ba71e14c4c1354a2cdb0
Florian Janietz <florianjanietz@MacBook-Pro-von-Florian.local>
2021-02-26 11:30:23 +0100

    Projekt erstellt

---
+++ src/test/java/de/storefinder/LocalStoreFinder/LocalStoreFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.storefinder.LocalStoreFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalStoreFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
