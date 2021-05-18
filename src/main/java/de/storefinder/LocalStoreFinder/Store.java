3ea40cc123c60e8842b4eff01fbcf5e14ee633d4
Julian Höfler <julhoefler@gmail.com>
2021-05-18 10:14:28 +0200

    added Repository for Store-Entity

--- src/main/java/de/storefinder/LocalStoreFinder/Store.java
+++ src/main/java/de/storefinder/LocalStoreFinder/Store.java
@@ -15,6 +15,10 @@ public class Store {
 
     private int zip;
 
+    private String name;
+
+    private String address;
+
     public void setId(Integer id) {
         this.id = id;
     }
@@ -29,5 +33,21 @@ public class Store {
 
     public void setZip(int zip) {
         this.zip = zip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
---
+++ src/main/java/de/storefinder/LocalStoreFinder/StoreRepository.java
@@ -0,0 +1,6 @@
+package de.storefinder.LocalStoreFinder;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StoreRepository extends CrudRepository<Store, Integer> {
+}
