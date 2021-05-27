0e57d03e85000f8b82fd31ea30f9a254bc6e0d15
Julian Höfler <julhoefler@gmail.com>
2021-05-27 13:23:20 +0200

    CodeReview

--- src/test/java/de/storefinder/LocalStoreFinder/mapper/StoreControllerTest.java
+++ src/test/java/de/storefinder/LocalStoreFinder/mapper/StoreControllerTest.java
@@ -2,6 +2,7 @@ package de.storefinder.LocalStoreFinder.mapper;
 
 import de.storefinder.LocalStoreFinder.controller.StoreController;
 import de.storefinder.LocalStoreFinder.models.requests.StoreInputModel;
+import de.storefinder.LocalStoreFinder.models.responses.PutOutputModel;
 import de.storefinder.LocalStoreFinder.repositories.*;
 import de.storefinder.LocalStoreFinder.services.StoreInputValidationService;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +12,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -50,11 +53,11 @@ public class StoreControllerTest {
     void createStore_ShouldReturn_BadRequest_AndNotSaveAnyDataInEntity_If_ValidationFailed() {
         StoreInputModel mockStoreInputModel = StoreInputModel.builder().build();
 
-        ResponseEntity<String> result = storeController.createStore(mockStoreInputModel);
+        ResponseEntity<PutOutputModel<String>> result = storeController.createStore(mockStoreInputModel);
 
         verify(storeRepository, never()).save(any());
 
-        assertThat(result.getBody()).isEqualTo("Request Data not correct");
+        assertThat(Objects.requireNonNull(result.getBody()).getMessage()).isEqualTo("Request Data not correct");
         assertThat(result.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
@@ -62,7 +65,7 @@ public class StoreControllerTest {
     void createStore_ShouldReturn_OK_AndSaveInputInEntity_If_ValidationSucceed() {
         StoreInputModel mockStoreInputModel = TestFactory.getTestStoreInputModelValid();
 
-        ResponseEntity<String> result = storeController.createStore(mockStoreInputModel);
+        ResponseEntity<PutOutputModel<String>> result = storeController.createStore(mockStoreInputModel);
 
         verify(addressRepository).save(any());
         verify(paymentRepository).save(any());
@@ -70,7 +73,7 @@ public class StoreControllerTest {
         verify(openingTimesRepository).save(any());
         verify(storeRepository).save(any());
 
-        assertThat(result.getBody()).isEqualTo("Successfully added to database.");
+        assertThat(Objects.requireNonNull(result.getBody()).getMessage()).isEqualTo("Store successfully added");
         assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
